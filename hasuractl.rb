359d2209ea7a569232965338a211f9f49b197bd8
Karthikeya Viswanath <karthikeya@hasura.io>
2017-09-20 20:29:27 +0530

    Init

--- hasuractl.rb
+++ hasuractl.rb
@@ -6,9 +6,15 @@ class Hasuractl < Formula
   sha256 "24b1e33d94d934b721442a052af61e04410f5f57"
 
   depends_on "git"
+  depends_on "kubernetes-cli"
 
   def install
     bin.install "hasuractl"
+  end
+
+  test do
+    run_output = shell_output("#{bin}/hasuractl version")
+    assert_match "hasuractl version: v0.1.12", run_output
   end
 end
 
